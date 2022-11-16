d5b318040ce6df33e7d672466311d2c06faa5aff
ikir0806 <ikir2826@gmail.com>
2022-11-16 11:29:55 +0300

    header completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Calendar } from './pages/Calendar';
+import { Categories } from './pages/Categories';
+import { Registration } from './pages/Registration';
+import { Operations } from './pages/Operations';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Categories />} />
+          <Route path='/operations' element={<Operations />} />
+          <Route path='/calendar' element={<Calendar />} />
+          <Route path='/registration' element={<Registration />} />
+        </Routes>
+    </div>
   );
 }
 

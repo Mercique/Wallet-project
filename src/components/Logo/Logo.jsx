fffbd834c73f92380209b59605d1af2bd329885a
dkhartsiy <danila.khartsiy@s-telematics.ru>
2022-11-23 18:46:47 +0300

    add logo link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,6 @@
 import styles from "./Logo.module.css";
 
 export function Logo() {
+  console.log('x')
   return <a href="/" className={styles.logoImage}><img src="/logo.svg" width={50} height={50} alt={'logo'}/></a>;
 }

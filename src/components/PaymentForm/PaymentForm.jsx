7eff3711daa7b61b8a0890659afc29c357bbd9d4
Serjik <serjik@MacBook-Air-Serjik.local>
2022-11-24 14:47:26 +0300

    fix Form component

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -1,31 +1,75 @@
 import "./PaymentForm.css";
 import { useState } from "react";
-import { Modal } from "../Modal/Modal";
 
-export const PaymentForm = ({addNewPayment, paymentList, categoryList}) => {
-  const [modalActive, setModalActive] = useState(false);
+export const PaymentForm = ({ addNewPayment }) => {  
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("Одежда");
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("UTC", options).format(today);
+  }
+
+  const resetForm = (e) => {
+    let inputsValues = e.target.elements;
+    inputsValues.name.value = ''
+    inputsValues.value.value = ''
+    inputsValues.date.value = ''
+    setName('');
+    setValue('');
+    setDate('');
+    setCategory('Одежда');
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
+    addNewPayment({
+      id: '',
+      name: name,
+      date: date ? date : getCurrentDate(),
+      category: category,
+      value: value,
+      img: ''
+    })
+    resetForm(e)
+  }
 
   return (
     <>
       <div className="content-wrapper">
         <form onSubmit={handleSubmit} className="payment-input-form">
-          <button
-            className="add_spend_button"
-            onClick={() => setModalActive(true)}
-          >
+          <div className="payment-input-form-">
+            <input className="expenses_input" type="number" name="value" placeholder="Введите сумму" onChange={(event) => setValue(event.target.value)}/>
+            <input className="expenses_input" type="text" name="name" placeholder="Введите название" onChange={(event) => setName(event.target.value)}/>
+          </div>
+          <div className="">
+            <select
+              className="expenses_input"
+              name="category"
+              value={category}
+              onChange={(event) => setCategory(event.target.value)}
+            >
+              <option id="0">Одежда</option>
+              <option id="1">Транспорт</option>
+              <option id="2">Кафе и рестораны</option>
+              <option id="3">Супермаркеты</option>
+              <option id="4">ЖКХ, связь, интернет</option>
+              <option id="5">Медицина</option>
+              <option id="6">Образование</option>
+              <option id="7">Прочие расходы</option>
+            </select>
+            <input className="expenses_input" type="date" name="date" onChange={(event) => setDate(event.target.value)}/>
+          </div>
+          <button disabled={!name | !value } className="add_spend_button">
             Добавить трату
           </button>
-          <Modal active={modalActive} setActive={setModalActive} addNewPayment={addNewPayment} paymentList={paymentList} categoryList={categoryList} />
-          <div className="filter-form">
-            <p className="filter-label">Фильтры:</p>
-            <button className="filter-btn">День</button>
-            <button className="filter-btn">Неделя</button>
-            <button className="filter-btn">Месяц</button>
-          </div>
         </form>
         <div className="balance">
           <p className="balance-label">Баланс</p>

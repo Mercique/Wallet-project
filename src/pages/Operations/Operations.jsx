0191eac66068d2b1ea5a717aa208897485a423f8
Ilya Chvanov <mercy1ess@list.ru>
2022-12-19 21:00:15 +0300

    Fix Chart.js view

--- src/pages/Operations/Operations.jsx
+++ src/pages/Operations/Operations.jsx
@@ -2,20 +2,22 @@ import styles from "./Operations.module.css";
 import { PaymentForm } from "../../components/PaymentForm/PaymentForm";
 import { Payment } from "../../components/Payment/Payment";
 import { TestSlider } from "../../components/TestSlider/TestSlider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Balance } from "../../components/Balance/Balance";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { sortPayments } from "../../store/payments/actions";
 import { apiPayments } from "../../utils/constants";
 import { PieChart } from "../../components/PieChart/PieChart";
+import { selectPayments } from "../../store/payments/selectors";
 
 export const Operations = () => {
   const [categoryEdit, setCategoryEdit] = useState("");
   const dispatch = useDispatch();
+  const paymentList = useSelector(selectPayments);
 
   const handleActive = (el) => {
     if (el.id !== categoryEdit?.id) {
-      setCategoryEdit({id: el.id, name: el.name, img_id: el.img_id});
+      setCategoryEdit({ id: el.id, name: el.name, img_id: el.img_id });
       dispatch(sortPayments(`${apiPayments}/${el.id}`));
     } else {
       setCategoryEdit("");
@@ -23,10 +25,48 @@ export const Operations = () => {
     }
   };
 
+  useEffect(() => {
+    if (!categoryEdit) {
+      dispatch(sortPayments(apiPayments));
+    }
+  }, [categoryEdit, dispatch]);
+
+  const getPayments = (list) => {
+    let arr = [];
+    let uniqObj = {};
+
+    for (let key of Object.keys(list)) {
+      arr.push(...list[key]);
+    }
+
+    arr.forEach((el) => {
+      uniqObj[el.categoryName] = (uniqObj[el.categoryName] || 0) + el.sum;
+    });
+
+    return uniqObj;
+  };
+
+  const pieList = getPayments(paymentList);
+
+  const chartData = {
+    labels: Object.keys(pieList).map((el) => el),
+    datasets: [
+      {
+        label: "Payments",
+        data: Object.values(pieList).map((el) => el),
+        borderColor: "transparent",
+      },
+    ],
+  };
+
   return (
     <div className={styles.operations}>
       <div className={styles.operationsLeft}>
-        <TestSlider className={styles.operationsTestSlider} categoryEdit={categoryEdit} handleActive={handleActive} />
+        <TestSlider
+          className={styles.operationsTestSlider}
+          categoryEdit={categoryEdit}
+          handleActive={handleActive}
+        />
         <div className={styles.operationsMiddle}>
           <PaymentForm />
           <Payment />
@@ -34,7 +74,7 @@ export const Operations = () => {
       </div>
       <div className={styles.operationsRight}>
         <Balance />
-        <PieChart />
+        <PieChart chartData={chartData} />
       </div>
     </div>
   );

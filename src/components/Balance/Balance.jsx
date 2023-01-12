d951c404db587a0800017a4083d02882e37049b3
dkhartsiy <danila.khartsiy@s-telematics.ru>
2023-01-12 17:05:44 +0300

    adaprive

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -2,11 +2,11 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../../store/profile/selectors";
 import styles from "./Balance.module.css";
 
-export const Balance = () => {
+export const Balance = ({ className }) => {
   const user = useSelector(selectUser);
   
   return (
-    <div className={styles.balance}>
+    <div className={`${styles.balance} ${className}`}>
       <p className={styles.balanceLabel}>Баланс:</p>
       <p className={styles.balanceValue}>{user?.balance?.toLocaleString()} &#8381;</p>
     </div>
